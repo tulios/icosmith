aa2af077ef776ddecf70f1d4a72138023d9b4c62
tulios <tulio@corp.globo.com>
2013-07-15 18:14:08 -0300

    skipping session and adding some cache to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+  def skip_session
+    request.session_options[:skip] = true
+  end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    skip_session
+    expires_in 15.minutes, public: true
   end
 
 end
