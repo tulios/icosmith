28e720298d1d1db14524acf9f489ed1306a0bf2b
Guilherme Garnier <guilherme.garnier@gmail.com>
2015-07-06 11:48:28 -0300

    Updating installation instructions for Tsuru dependencies

--- lib/install.sh
+++ lib/install.sh
@@ -1,15 +1,15 @@
-mkdir /build && \
-cd /build && \
+mkdir /tmp/build && \
+cd /tmp/build && \
 tar zxvf /home/application/current/lib/ttf2eot-0.0.2-2.tar.gz && \
-sed -i.bak "/using std::vector;/ i\#include <cstddef>" /build/ttf2eot-0.0.2-2/OpenTypeUtilities.h && \
-cd /build/ttf2eot-0.0.2-2 && \
+sed -i.bak "/using std::vector;/ i\#include <cstddef>" /tmp/build/ttf2eot-0.0.2-2/OpenTypeUtilities.h && \
+cd /tmp/build/ttf2eot-0.0.2-2 && \
 make && \
 cp ttf2eot /usr/local/bin/ttf2eot
 
 ## ttfautohint build
-cd /build && \
+cd /tmp/build && \
 tar zxvf /home/application/current/lib/ttfautohint-0.95.tar.gz && \
-cd /build/ttfautohint-0.95 && \
+cd /tmp/build/ttfautohint-0.95 && \
 ./configure --with-qt=no --without-doc && \
 make && \
 make install
