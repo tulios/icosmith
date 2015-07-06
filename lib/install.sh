9c1c2c1ed94c17b40b9fd4bc706f8cc0044335d6
Guilherme Garnier <guilherme.garnier@gmail.com>
2015-07-06 17:46:48 -0300

    Trying to install dependencies with sudo

--- lib/install.sh
+++ lib/install.sh
@@ -4,14 +4,12 @@ tar zxvf /home/application/current/lib/ttf2eot-0.0.2-2.tar.gz && \
 sed -i.bak "/using std::vector;/ i\#include <cstddef>" /tmp/build/ttf2eot-0.0.2-2/OpenTypeUtilities.h && \
 cd /tmp/build/ttf2eot-0.0.2-2 && \
 make && \
-cp ttf2eot /home/application/current/ttf2eot
+sudo cp ttf2eot /usr/local/bin/ttf2eot
 
 ## ttfautohint build
 cd /tmp/build && \
 tar zxvf /home/application/current/lib/ttfautohint-0.95.tar.gz && \
 cd /tmp/build/ttfautohint-0.95 && \
-./configure --with-qt=no --without-doc --prefix=/home/application/current/ttfautohint && \
+./configure --with-qt=no --without-doc && \
 make && \
-make install
-
-export PATH=$PATH:/home/application/current/ttfautohint/bin:/home/application/current/ttf2eot/bin
+sudo make install
